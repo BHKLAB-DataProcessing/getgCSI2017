07d5a764dfa36b14657ba87af8310c9340867795
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-04-09 15:43:56 -0400

    Update getgCSI2017.R

--- getgCSI2017.R
+++ getgCSI2017.R
@@ -413,27 +413,28 @@ standardizeRawDataConcRange <- function(sens.info, sens.raw){
 		 
 		 
 #add cellosaurus disease type to cell-info
-disease <- cell_all$Cellosaurus.Disease.Type[match(cellInfo$cellid, cell_all$unique.cellid)]
+colnames(cellInfo)[which(names(cellInfo) == "cellid")] <- "unique.cellid"
+disease <- cell_all$Cellosaurus.Disease.Type[match(cellInfo$unique.cellid, cell_all$unique.cellid)]
 cellInfo$Cellosaurus.Disease.Type <- disease
 		 
 #add cellosaurus assession to cell-info
-assession <- cell_all$Cellosaurus.Accession.id[match(cellInfo$cellid, cell_all$unique.cellid)]
+assession <- cell_all$Cellosaurus.Accession.id[match(cellInfo$unique.cellid, cell_all$unique.cellid)]
 cellInfo$Cellosaurus.Accession.id <- assession
 		 
 #add pharmacodb id to cell-info
-pdb <- cell_all$PharmacoDB.id[match(cellInfo$cellid, cell_all$unique.cellid)]
+pdb <- cell_all$PharmacoDB.id[match(cellInfo$unique.cellid, cell_all$unique.cellid)]
 cellInfo$PharmacoDB.id <- pdb
 
 #add study tissue id to cell_info
-study_tissue <- cell_all$unique.tissueid.fromstudies[match(cellInfo$cellid, cell_all$unique.cellid)]
+study_tissue <- cell_all$unique.tissueid.fromstudies[match(cellInfo$unique.cellid, cell_all$unique.cellid)]
 cellInfo$unique.tissueid.fromstudies <- study_tissue
 		 
 #add study cell-line type to cell_info
-cell_type <- cell_all$CellLine.Type[match(cellInfo$cellid, cell_all$unique.cellid)]
+cell_type <- cell_all$CellLine.Type[match(cellInfo$unique.cellid, cell_all$unique.cellid)]
 cellInfo$CellLine.Type <- cell_type
 		 
 #add metastatic info to cell_info		 
-metastatic <- cell_all$Metastatic[match(cellInfo$cellid, cell_all$unique.cellid)]
+metastatic <- cell_all$Metastatic[match(cellInfo$unique.cellid, cell_all$unique.cellid)]
 cellInfo$Metastatic <- metastatic
 		 
 		 
